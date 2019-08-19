5353e78dce648009341d6a7ce84b6b5721dc1d16
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-19 20:54:55 +0530

    Added form data parsing methods

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const Express=require('express');
 var app = new Express();
+var bodyParser=require('body-parser');
+
 app.set('view engine','ejs');
+
+app.use(bodyParser.json());
 app.use(Express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/',(rq,rs)=>{
     rs.render('index',{title:"MY website"});
 });
@@ -10,5 +16,15 @@ app.get('/login',(rq,rs)=>{
 });
 app.get('/reg',(rq,rs)=>{
     rs.render('reg')
+});
+app.post('/read',(rq,rs)=>{
+    var items=rq.body
+    rs.render('read',{item:items});
+    var name=rq.body.n; //n = names's input name
+    var pass=rq.body.p; //p=password's input name
+    var array=rq.body;
+    console.log(name);
+    console.log(pass);
+    console.log(array);
 });
 app.listen( process.env.PORT||3001);
