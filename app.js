6e917a11ddccd89e06adefcbe1b09b6b87b8e739
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-21 07:10:00 +0530

    database added and operations done

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const Express=require('express');
+const Mongoose=require('mongoose');
+var request =require('request');
 var app = new Express();
 var bodyParser=require('body-parser');
 
 app.set('view engine','ejs');
 
 app.use(bodyParser.json());
-app.use(Express.static(__dirname+"/public"));
+app.use(Express.static(__dirname+"/public"));//public files css,images accessing
 app.use(bodyParser.urlencoded({extended:true}));
+
+Mongoose.connect("mongodb://localhost:27017/studentdb1")
+
+const StudentModel=Mongoose.model("studentdetails",{
+    n:String,
+    a:String,
+    gender:String,
+    district:String,
+    dob:String,
+    email:String,
+    uname:String,
+    upass:String,
+    upass1:String
+})
 
 app.get('/',(rq,rs)=>{
     rs.render('index',{title:"MY website"});
@@ -17,14 +33,135 @@ app.get('/login',(rq,rs)=>{
 app.get('/reg',(rq,rs)=>{
     rs.render('reg')
 });
-app.post('/read',(rq,rs)=>{
+app.post('/loginview',(rq,rs)=>{
     var items=rq.body
-    rs.render('read',{item:items});
+    rs.render('loginview',{item:items});
     var name=rq.body.n; //n = names's input name
-    var pass=rq.body.p; //p=password's input name
+    var pass=rq.body.p; //p= password's input name
     var array=rq.body;
     console.log(name);
     console.log(pass);
     console.log(array);
 });
+
+
+app.post('/read',(req,res)=>{
+    //var items=req.body;
+    //res.render('read',{item:items});
+
+    var student = StudentModel(req.body);
+    var result = student.save((error,data)=>{                          //api to send data to database
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send("<script>alert('Employee Successfully Inserted')</script>");
+        }
+    });
+
+});
+
+
+app.get('/studentall',(req,res)=>{
+
+    var result = StudentModel.find((error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);                                            //api recieve data from database
+        }
+        else
+        {
+            res.send(data);
+        }
+    });
+});
+
+
+
+const APIurl ="http://localhost:3001/studentall"
+
+ 
+    app.get('/viewstudents',(req,res)=>{
+
+        request(APIurl,(error,response,body)=>{
+            var data = JSON.parse(body);
+            res.render('viewstudents',{data:data});
+        });
+    });
+    
+
+    app.get('/searchstudent',(req,res)=>{
+        res.render('searchstudent');
+    });
+
+
+    app.get('/studentname',(req,res)=>{
+        var item = req.query.n;
+        //app.get('/emplyee/:id)
+        //item=req.params.id;
+        //var item = req.body.ename;
+        //console.log(item);
+        var result = StudentModel.find({n:item},(error,data)=>{
+            if(error)
+            {
+                throw error;
+                res.send(error);
+            }
+            else
+            {
+                res.send(data);
+            }
+        })
+    
+    });
+
+    const APIurl2 ="http://localhost:3001/studentname"
+    app.post('/viewsinglestudent',(req,res)=>{
+
+        var item = req.body.n;
+    
+        request(APIurl2+"/?n="+item,(error,response,body)=>{
+            var data = JSON.parse(body);
+            res.render('viewsingle',{data:data});
+        })
+    });
+
+    app.get('/deletestudent',(req,res)=>{
+        res.render('deletestudent');
+    });
+
+    app.get('/deleteAPI',(req,res)=>{
+        var item= req.query.n;
+    
+        var result = StudentModel.deleteOne({n:item},(error,data)=>{
+            if(error)
+            {
+                throw error;
+                res.send(error);
+            }
+            else
+            {
+                res.send(data);
+            }
+        })
+    })
+
+
+    const APIurl3 = "http://localhost:3001/deleteAPI"
+
+    app.post('/studdelete',(req,res)=>{
+        var item = req.body.n;
+    
+        request(APIurl3+"/?n="+item,(error,response,body)=>{
+    
+            res.send("<script>alert('student Deleted')</script><script>window.location.href='/deletestudent'</script>");
+    
+        })
+    });
+
+
 app.listen( process.env.PORT||3001);
